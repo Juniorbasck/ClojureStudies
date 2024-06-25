69f8dd92c087ece3def58b29e7985700273f9b8b
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-06-24 23:24:17 -0300

    vetores

---
+++ vetores.clj
@@ -0,0 +1,23 @@
+[1 2 3 4 5 6 7 8 9]
+
+
+[1 2 3 true false "He4rt Developers"]
+
+(def desenvolvedores
+     ["Junior" "Daniel" "tiago" "mathtues" "jessica"])
+
+
+
+(first desenvolvedores)
+
+(rest desenvolvedores)
+
+(nth desenvolvedores 3)
+
+(count desenvolvedores)
+
+(conj desenvolvedores "Guto")
+
+(cons "Guto" desenvolvedores)
+
+(println desenvolvedores)
