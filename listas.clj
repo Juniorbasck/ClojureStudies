33b09240630a3e05f9716c3c6a98e7226f2a102e
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-06-24 23:26:26 -0300

    listas

---
+++ listas.clj
@@ -0,0 +1,15 @@
+'(1 2 3 4 5)
+
+(def lista-desenvolvedores
+    '("Kalane" "Daniel" "Cherry" "Canhassi" "Fabrício"))
+
+(count lista-desenvolvedores)
+
+(first lista-desenvolvedores)
+
+
+(rest lista-desenvolvedores)
+
+(list-desenvolvedores 3)
+
+(conj lista-desenvolvedores "Guto")
