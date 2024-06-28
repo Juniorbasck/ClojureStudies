6c74b9a4053a703803d9c8c25ad04f91ef4316c0
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-06-28 09:48:40 -0300

    estrutura for

---
+++ estruturasReps/for.clj
@@ -0,0 +1,13 @@
+(for [i (range 10)]
+      (println i))
+
+(def numeros [0 1 2 3 4 5 6 7 8 9])
+
+(for [i numeros]
+      (println i))
+
+(def letras ["a" "b" "c" "d" "e"])
+
+(for [l letras]
+  (println l))
+
