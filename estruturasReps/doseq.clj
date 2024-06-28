ead4c6b2bfde981c2662243f51a5971ede9f9a8f
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-06-28 12:02:12 -0300

    atualizacoes

--- estruturasReps/doseq.clj
+++ estruturasReps/doseq.clj
@@ -1,1 +1,14 @@
-aaa:wq
+(def numeros [0 1 2 3 4 5 6 7 8 9])
+
+(doseq [i numeros]
+      (println i))
+
+;; Resultado:
+;;  1
+;;  2
+;;  3
+;;  4
+;;  5
+;;  nil
+
+
---
+++ estruturasReps/loop.clj
@@ -0,0 +1,4 @@
+(loop [i 1]
+  (when (<= i 10)
+    (println i)
+    (recur (inc i))))
