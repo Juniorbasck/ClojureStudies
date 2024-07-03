8ebdb703d6fdb1e65429c681b6144633ce73e732
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-07-03 10:45:34 -0300

    updates

---
+++ clojureScript/serverMoc/server/src/server/core.clj
@@ -0,0 +1,33 @@
+(ns server.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [com.moclojer.adapters :as adapters]
+            [com.moclojer.server :as server]) 
+  (:gen-class))
+
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str "TESTE TESTE")})
+
+(def *router
+  "create a router from a config map"
+  (adapters/generate-routes
+   [{:endpoint
+     {:method "GET"
+      :path "/example"
+      :response {:status 200
+                 :headers {:Content-Type "application/json"}
+                 :body {:id 123}}}}]))
+
+
+
+(defn -main
+  "start the server"
+  [& args]
+  (server/start-server! *router))
+
+(defn -main [& args]
+  (run-server app {:port 3000})
+  (println "Server inicializado na porta 3000"))
+
